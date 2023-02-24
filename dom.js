cc18035d7539b9354ec9526386788e161ec376a2
suryansh2206 <suryansh804.sg@gmail.com>
2023-02-25 00:41:25 +0530

    DOM(index)

---
+++ dom.js
@@ -0,0 +1,15 @@
+ //console.log(document.domain);
+ //console.log(document.URL);
+ //console.log(document.title);
+ //console.log(document.doctype);
+ //console.log(document.head);
+
+ var headerTitle = document.getElementById('header-title');
+ var header = document.getElementById('main-header');
+
+ header.style.borderBottom = 'solid 3px #000';
+
+ var title = document.getElementsByClassName('title')[0];
+ title.style.fontWeight = 'bold';
+ title.style.color = 'green';
+ 
