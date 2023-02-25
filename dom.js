c7d2eff37a6750cfb3a367a834d8399390a82f75
suryansh2206 <suryansh804.sg@gmail.com>
2023-02-25 15:55:06 +0530

    QuerySelector & QuerySelectorALL

--- dom.js
+++ dom.js
@@ -13,12 +13,30 @@
 //  title.style.fontWeight = 'bold';
 //  title.style.color = 'green';
 
- var items = document.getElementsByClassName('list-group-item');
-//  console.log(items);
-items[2].style.backgroundColor = 'green';
- for(var i = 0; i<items.length; i++)
- {
-    items[i].style.fontWeight = 'bold';
- }
-var li = document.getElementsByTagName('li');
-li[4].style.backgroundColor = 'red';
+//  var items = document.getElementsByClassName('list-group-item');
+// //  console.log(items);
+// items[2].style.backgroundColor = 'green';
+//  for(var i = 0; i<items.length; i++)
+//  {
+//     items[i].style.fontWeight = 'bold';
+//  }
+// var li = document.getElementsByTagName('li');
+// li[4].style.backgroundColor = 'red';
+
+//QUERYSELECTOR
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display = 'none';
+
+//QUERYSELECTORALL
+var items = document.querySelectorAll('.list-group-item');
+// console.log(items);
+items[1].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i = 0; i < odd.length; i++)
+{
+    odd[i].style.backgroundColor = 'green';
+}
