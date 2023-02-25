a1339744ecb35163dd4803aefa65695c98cd45bc
suryansh2206 <suryansh804.sg@gmail.com>
2023-02-25 14:45:31 +0530

    background color and bold(getElementsByClassName)

--- dom.js
+++ dom.js
@@ -4,12 +4,19 @@
  //console.log(document.doctype);
  //console.log(document.head);
 
- var headerTitle = document.getElementById('header-title');
- var header = document.getElementById('main-header');
+//  var headerTitle = document.getElementById('header-title');
+//  var header = document.getElementById('main-header');
 
- header.style.borderBottom = 'solid 3px #000';
+//  header.style.borderBottom = 'solid 3px #000';
 
- var title = document.getElementsByClassName('title')[0];
- title.style.fontWeight = 'bold';
- title.style.color = 'green';
- 
+//  var title = document.getElementsByClassName('title')[0];
+//  title.style.fontWeight = 'bold';
+//  title.style.color = 'green';
+
+ var items = document.getElementsByClassName('list-group-item');
+//  console.log(items);
+items[2].style.backgroundColor = 'green';
+ for(var i = 0; i<items.length; i++)
+ {
+    items[i].style.fontWeight = 'bold';
+ }
